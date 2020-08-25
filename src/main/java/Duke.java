bd0a814fb5f3a873cdb238a3d7ed7c96ad91e997
Dash Abhishek <abhishekdsh8@gmail.com>
2020-08-25 23:23:55 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        String line = " ";
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -10,6 +16,17 @@ public class Duke {
         System.out.println(lineLogo);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+        System.out.println(lineLogo);
+
+        while(!line.equals("bye")){
+            line = in.nextLine();
+            if(!line.equals("bye")){
+                System.out.println(lineLogo);
+                System.out.println(line);
+                System.out.println(lineLogo);
+            }
+        }
+
         System.out.println(lineLogo);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(lineLogo);
