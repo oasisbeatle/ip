a1d347cbe237a37f91f5af08b8a95ba4b0b4916d
Dash Abhishek <abhishekdsh8@gmail.com>
2020-08-26 00:47:05 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,6 +7,10 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         String line = " ";
         ArrayList<String> list = new ArrayList<String>();
+        String taskComplete = Character.toString((char) 0x2713);
+        String taskIncomplete = Character.toString((char) 0x274C);
+        ArrayList<String> listCompletion = new ArrayList<String>();
+
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -22,17 +26,30 @@ public class Duke {
 
         while(!line.equals("bye")){
             line = in.nextLine();
-            if(!line.equals("bye") && !line.equals("list")){
+            if(!line.equals("bye") && !line.equals("list") && !line.startsWith("done")){
                 list.add(line);
+                listCompletion.add(taskIncomplete);
                 System.out.println(lineLogo);
                 System.out.println("added: " + line);
                 System.out.println(lineLogo);
             }
             else if(line.equals("list")){
                 System.out.println(lineLogo);
+                System.out.println("Here are the tasks in your list:");
                 for(int i = 0; i < list.size(); i++){
-                    System.out.println(i + ". " + list.get(i));
+                    System.out.println((i+1) + ".[" + listCompletion.get(i) + "] " + list.get(i));
                 }
+                System.out.println(lineLogo);
+            }
+
+            else if(line.startsWith("done")){
+                String taskString = line.substring(5);
+                int taskNum = Integer.parseInt(taskString);
+                taskNum--;
+                listCompletion.set(taskNum, taskComplete);
+                System.out.println(lineLogo);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("   ["  + listCompletion.get(taskNum) + "] "+ list.get(taskNum));
                 System.out.println(lineLogo);
             }
         }
