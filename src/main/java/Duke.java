b06302599eebf7840c86010b737e89ca389c89ef
Dash Abhishek <abhishekdsh8@gmail.com>
2020-08-25 23:58:45 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
         String line = " ";
+        ArrayList<String> list = new ArrayList<String>();
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -20,9 +22,17 @@ public class Duke {
 
         while(!line.equals("bye")){
             line = in.nextLine();
-            if(!line.equals("bye")){
+            if(!line.equals("bye") && !line.equals("list")){
+                list.add(line);
                 System.out.println(lineLogo);
-                System.out.println(line);
+                System.out.println("added: " + line);
+                System.out.println(lineLogo);
+            }
+            else if(line.equals("list")){
+                System.out.println(lineLogo);
+                for(int i = 0; i < list.size(); i++){
+                    System.out.println(i + ". " + list.get(i));
+                }
                 System.out.println(lineLogo);
             }
         }
